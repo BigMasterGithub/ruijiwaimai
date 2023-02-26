a3cc8cf5a7db15f3261fa976e809f172ff04a294
BigWang <2570187790@qq.com>
2023-02-26 11:24:21 +0800

    添加常量类,防止魔法值

---
+++ src/main/java/com/itheima/reggie/utils/AAAConstans.java
@@ -0,0 +1,12 @@
+package com.itheima.reggie.utils;
+
+/**
+ * @author 张壮
+ * @description TODO
+ * @since 2023/2/25 14:43
+ **/
+public class AAAConstans {
+    public static final String DEFAULT_PASS = "123456";
+
+}
+
